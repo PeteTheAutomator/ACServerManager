8b38be55b6641707e6b05aff50a08684347e7ec9
pete <peter.hehn@yahoo.com>
2016-08-04 13:31:59 +0100

    fixed STATIC_ROOT

--- acserver/settings.py
+++ acserver/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/usr/local/ACServerManager'
+STATIC_ROOT = '/usr/local/ACServerManager/static'
