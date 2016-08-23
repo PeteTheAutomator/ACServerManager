785f84a224dc119c1faef7b5b37c9f78962731ef
pete <peter.hehn@yahoo.com>
2016-08-23 18:18:30 +0100

    forgot to add migration

---
+++ library/migrations/0002_document.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('library', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Document',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('docfile', models.FileField(upload_to=b'documents/%Y/%m/%d')),
+            ],
+        ),
+    ]
