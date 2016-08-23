7d81c64cbd32478d42dd1ae2bfd0e585b122d33a
pete <peter.hehn@yahoo.com>
2016-08-23 18:16:57 +0100

    further iteration for importing assets

--- library/urls.py
+++ library/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^document/(?P<preset_id>[0-9]+)/process/$', views.process_document, name='process_document'),
+    url(r'^document/(?P<document_id>[0-9]+)/process/$', views.process_document, name='process_document'),
 ]
