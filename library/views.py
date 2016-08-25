a315a2e64a0c1c1ec539f0260e33f89f1c5d4c4d
pete <peter.hehn@yahoo.com>
2016-08-25 12:05:01 +0100

    securing  custom views

--- library/views.py
+++ library/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 from .models import AssetCollection
 from .tasks import process_assets
 from time import sleep
 
 
+@login_required
 def process_assetcollection(request, assetcollection_id):
     # TODO: make it pretty (yeah - it's horrible, but let's get something vaguely working)
     process_assets(assetcollection_id)
--- session/views.py
+++ session/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 from .models import Preset
 from .tasks import write_config, kick_services, get_server_status, stop_services
 from time import sleep
 
 
+@login_required
 def launch_preset(request, preset_id):
     preset = Preset.objects.get(id=preset_id)
     write_config(preset_id=preset.id)
@@ -22,6 +24,7 @@ def launch_preset(request, preset_id):
     return redirect('/admin/session/preset/')
 
 
+@login_required
 def stop_preset(request, preset_id):
     # TODO: make it pretty (yeah - it's horrible, but let's get something vaguely working)
     stop_services(preset_id)
