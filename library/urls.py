aafdc7586f677e4788f73dbc4090f8e4bbea709a
pete <peter.hehn@yahoo.com>
2016-08-25 11:29:50 +0100

    fixing name clash on process_assets

--- library/urls.py
+++ library/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^assetcollection/(?P<assetcollection_id>[0-9]+)/process/$', views.process_assets, name='process_assets'),
+    url(r'^assetcollection/(?P<assetcollection_id>[0-9]+)/process/$', views.process_assetcollection, name='process_assetcollection'),
 ]
--- library/views.py
+++ library/views.py
@@ -4,7 +4,7 @@ from .tasks import process_assets
 from time import sleep
 
 
-def process_assets(request, assetcollection_id):
+def process_assetcollection(request, assetcollection_id):
     # TODO: make it pretty (yeah - it's horrible, but let's get something vaguely working)
     process_assets(assetcollection_id)
     return redirect('/admin/library/assetcollection/')
