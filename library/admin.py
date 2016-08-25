a3432ef5f6192daf61df9a882805e068e245bbf4
pete <peter.hehn@yahoo.com>
2016-08-25 11:44:16 +0100

    added a ACSERVER_FULL_ADMIN_VIEW boolean to settings, which controls full or partial registration of objects for superuser in the admin view

--- library/admin.py
+++ library/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Car, CarSkin, CarTag, Track, TrackDynamism, Weather, AssetCollection
+from background_task.models import Task, CompletedTask
+from django.conf import settings
 
 
 class DocumentAdmin(admin.ModelAdmin):
@@ -12,10 +14,15 @@ class DocumentAdmin(admin.ModelAdmin):
     process_assets.allow_tags = True
 
 
-#admin.site.register(Car)
-#admin.site.register(CarSkin)
-#admin.site.register(CarTag)
-#admin.site.register(Track)
-#admin.site.register(TrackDynamism)
-#admin.site.register(Weather)
-admin.site.register(AssetCollection, DocumentAdmin)
+if settings.ACSERVER_FULL_ADMIN_VIEW:
+    admin.site.register(Car)
+    admin.site.register(CarSkin)
+    admin.site.register(CarTag)
+    admin.site.register(Track)
+    admin.site.register(TrackDynamism)
+    admin.site.register(Weather)
+    admin.site.register(AssetCollection, DocumentAdmin)
+else:
+    admin.site.register(AssetCollection, DocumentAdmin)
+    admin.site.unregister(Task)
+    admin.site.unregister(CompletedTask)
