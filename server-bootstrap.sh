a86b3a61705c8f35dc78fb674b62e3b6318782cf
pete <peter.hehn@yahoo.com>
2016-08-25 10:21:40 +0100

    cloud-init runs as root, dummy

--- server-bootstrap.sh
+++ server-bootstrap.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/bash
 # Bootstrap script for the Assetto Corsa Server Manager
 
-sudo sed -i 's/^Defaults\s\+requiretty/Defaults    !requiretty/' /etc/sudoers
-sudo yum install -y python-setuptools python-devel policycoreutils-python openssl-devel git gcc
-sudo easy_install pip
-sudo pip install ansible
+yum install -y python-setuptools python-devel policycoreutils-python openssl-devel git gcc
+easy_install pip
+pip install ansible
 
-git clone https://github.com/PeteTheAutomator/ACServerManager.git
-cd ACServerManager/ansible
+git clone https://github.com/PeteTheAutomator/ACServerManager.git /var/tmp
+cd /var/tmp/ACServerManager/ansible
 
 cat > ./vars.yml <<EOL
 superuser_name: $NAME
